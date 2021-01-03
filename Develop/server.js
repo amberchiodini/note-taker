9117b091e12059263e9d0486f3ecda05ba173468
amberchiodini <69092983+amberchiodini@users.noreply.github.com>
2021-01-03 08:15:15 -0600

    Initial commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 //Dependencies
 const express = require("express"); 
+const fs = require("fs");
+const path = require("path");
 
 //Set-up the express app
 const app = express();
@@ -10,11 +12,10 @@ const PORT = process.env.PORT || 3000
 //Set-up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/'));
 
-//Setup html and api routes
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
+//Set-up html and api routes
+require("./routes/routes")(app);
 
 //Start the server to begin listening
 app.listen(PORT, function () {
